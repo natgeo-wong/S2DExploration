b2595c8e9403ff28daa6e1b0f72ab6a18b7cc4e4
Nathanael Wong <natgeo.wong@outlook.com>
2025-08-13 11:08:16 -0400

    Now this is a template and now we have info on the station we want to extract info from

--- scripts/dkrz/extractepre.jl
+++ scripts/dkrz/extractepre.jl
@@ -1,6 +1,7 @@
 using DrWatson
 @quickactivate "S2DExploration"
 using ERA5Reanalysis
+using DelimitedFiles
 using Logging
 
 include(srcdir("common.jl"))
@@ -9,14 +10,12 @@ e5ds = ERA5Hourly(start=Date(2001),stop=Date(2024,12,31),path=datadir())
 evar = PressureVariable("t")
 
 dtvec = e5ds.start : Day(1) : e5ds.stop; ndt = length(dtvec)
-gID =
-lon =
-lat =
+gID,lon,lat = readdlm("info.txt",',',comments=true,comment_char='#')
 ipnt = closestnativelonlat(Point2(lon,lat))
 vmat = zeros(37,24*ndt)
 
 for idt in 1 : ndt
-    @info "$(now()) - S2DExploration - Extraction point data $(evar.name) for ($(lon),$(lat)) from the DKRZ servers"
+    @info "$(now()) - S2DExploration - Extraction point data $(evar.name) for ($(lon),$(lat)) from the DKRZ servers for $(dtvec[idt])"
     ibeg = (idt-1) * 24 + 1
     iend = idt * 24
     gds = dkrz(e5ds,evar,dtvec[idt])
