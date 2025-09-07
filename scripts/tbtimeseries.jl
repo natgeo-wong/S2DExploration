539c66c26be24430b61b3ff6afb7e13100de4c96
Nathanael Wong <natgeo.wong@outlook.com>
2025-09-06 22:00:32 -0400

    Some updates

--- scripts/tbtimeseries.jl
+++ scripts/tbtimeseries.jl
@@ -1,7 +1,8 @@
 using DrWatson
 @quickactivate "S2DExploration"
 
-using NASAMergedTb
+using RegionGrids, NASAMergedTb
+using DelimitedFiles
 
 geo = GeoRegion("KS_OK",path=srcdir())
 tbd = TbDataset(start=Date(2001),stop=Date(2024,12,31),path=datadir())
@@ -9,20 +10,22 @@ tbd = TbDataset(start=Date(2001),stop=Date(2024,12,31),path=datadir())
 dtvec = tbd.start : Day(1) : tbd.stop; ndt = length(dtvec)
 tbdata = zeros(Float32,48,ndt)
 
+gID,slon,slat = readdlm("info.txt",',',comments=true,comment_char='#')
+
 lon,lat = NASAMergedTb.btdlonlat()
 ggrd = RegionGrid(geo,lon,lat)
-ilon = argmin.(slon.-ggrd.lon)
-ilat = argmin.(slat.-ggrd.lat)
+ilon = argmin(abs.(slon.-ggrd.lon))
+ilat = argmin(abs.(slat.-ggrd.lat))
 
 for idt = 1 : ndt
 
     ids = read(tbd,geo,dtvec[idt])
-    tbdata[:,idt] = nomissing(ids["tb"][ilon,ilat,:],NaN)
+    tbdata[:,idt] = nomissing(ids["Tb"][ilon,ilat,:],NaN)
     close(ids)
 
 end
 
-fnc = joinpath(tbd.path,"SGP-Tb-$(Dates.format(tbd.start,dateformat"yyyymmdd"))-$(Dates.format(tbd.stop,dateformat"yyyymmdd")).nc")
+fnc = joinpath(tbd.path,"$gID-Tb-$(Dates.format(tbd.start,dateformat"yyyymmdd"))-$(Dates.format(tbd.stop,dateformat"yyyymmdd")).nc")
 if isfile(fnc); rm(fnc,force=true) end
 
 ds = NCDataset(fnc,"c",attrib = Dict(
@@ -38,7 +41,7 @@ nctime = defVar(ds,"valid_time",Int64,("valid_time",),attrib = Dict(
     "calendar"  => "gregorian",
 ))
 
-ncvar = defVar(pds,"Tb",Float32,("valid_time",),attrib = Dict(
+ncvar = defVar(ds,"Tb",Float32,("valid_time",),attrib = Dict(
     "units"         => "K",
     "standard_name" => "brightness_temperature",
     "full_name"     => "Brightness Temperature",
